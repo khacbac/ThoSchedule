65bb1e78a2a8e627e28161024ab4099aeb57bd08
Khac Bac <hokhacbac@gmail.com>
2019-07-11 13:27:26 +0700

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  "rules": {
+    "quotes": [1, "double", "avoid-escape"],
+    "comma-dangle": ["error", "never"],
+  }
+};
